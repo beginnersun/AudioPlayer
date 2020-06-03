91b352e811a740db1d283311a61be250846f9539
beginnersun <1985848358@qq.com>
2020-06-03 17:30:46 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/audioplayer/MainActivity.kt
+++ app/src/main/java/com/example/audioplayer/MainActivity.kt
@@ -51,5 +51,33 @@ class MainActivity : AppCompatActivity(),ViewPager.OnPageChangeListener {
         oldMenuItem = navigation_bottom.menu[position]
     }
 
+class MyListAdapter(private val context: Context,private val strings:MutableList<String>) : BaseAdapter(){
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val viewHolder:ViewHolder
+            val view:View
+            if (convertView != null){
+                viewHolder = convertView.tag as ViewHolder
+                view = convertView
+            }else{
+                view = LayoutInflater.from(context).inflate(R.layout.item_group,parent,false)
+                viewHolder = ViewHolder(view)
+                view.tag = viewHolder
+            }
+            viewHolder.tvContent.text = getItem(position) as String
+            return view
+        }
 
+        override fun getItem(position: Int): Any =
+            strings[position]
+
+
+        override fun getItemId(position: Int): Long = position.toLong()
+
+        override fun getCount(): Int = strings.size
+
+        class ViewHolder(itemView:View){
+            val tvContent: AppCompatTextView = itemView.findViewById(R.id.tv_content)
+
+        }
+    }
 }
