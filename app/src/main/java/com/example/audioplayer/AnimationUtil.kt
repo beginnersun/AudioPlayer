90adf7e0a4f33b759968bc468171986bf18a4b02
beginnersun <1985848358@qq.com>
2020-06-03 16:55:30 +0800

    Update AnimationUtil.kt

--- app/src/main/java/com/example/audioplayer/AnimationUtil.kt
+++ app/src/main/java/com/example/audioplayer/AnimationUtil.kt
@@ -5,14 +5,16 @@ import android.view.animation.LinearInterpolator
 import android.view.animation.RotateAnimation
 
 fun getRotateAnimation(duration: Long,pivotX:Float,pivotY: Float,fromDegrees :Float = 0f,toDegrees:Float = 180f) =
-    RotateAnimation(fromDegrees,toDegrees,fromDegrees,toDegrees).apply {
+    RotateAnimation(fromDegrees,toDegrees,pivotX,pivotY).apply {
         interpolator = LinearInterpolator()
         setDuration(duration)
+        fillAfter = true
     }
 
 fun rotate180(duration:Long,view:View){
-    val startRotate = if (view.tag == null) 0f else 180f
+    val startRotate = if (view.tag == null || (view.tag as Float) == 180f) 0f else 180f
     val rotateAnimation = getRotateAnimation(duration,view.pivotX,view.pivotY,startRotate,startRotate+180f)
     view.tag = startRotate
-    view.animation = rotateAnimation
+    view.startAnimation(rotateAnimation)
+
 }
